ba7ef3c1dabe9205136763591befbcd65e30c44c
mrpineappple1 <128257965+mrpineappple1@users.noreply.github.com>
2024-01-22 22:22:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('backcolor').addEventListener('input', backCol);
-});
-
-document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('text').addEventListener('input', textCol);
-});
-
-document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('prim').addEventListener('input', primCol);
-});
-
-document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('second').addEventListener('input', secondCol);
-});
-
-document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('accent').addEventListener('input', accentCol);
 });
 
@@ -22,28 +10,33 @@ function backCol() {
   var color = document.getElementById('backcolor').value;
   document.getElementById("example").style.background = color;
   document.getElementById('background').innerHTML = color;
+  document.querySelector(".dback").style.background = color;
 }
 
 function textCol() {
   var color2 = document.getElementById('text').value;
   document.getElementById("example").style.color = color2;
   document.getElementById('textcol').innerHTML = color2;
+  document.querySelector(".dtext").style.background = color2;
 }
 
 function primCol() {
   var color3 = document.getElementById('prim').value;
   document.querySelector(".btn1").style.background = color3;
   document.getElementById('primcol').innerHTML = color3;
+  document.querySelector(".dprim").style.background = color3;
 }
 
 function secondCol() {
   var color4 = document.getElementById('second').value;
   document.querySelector(".btn2").style.background = color4;
   document.getElementById('secondcol').innerHTML = color4;
+  document.querySelector(".dsecond").style.background = color4;
 }
 
 function accentCol() {
   var color5 = document.getElementById('accent').value;
   document.getElementById('accentcol').innerHTML = color5;
   document.getElementById('below').style.background = color5;
+  document.querySelector(".daccent").style.background = color5;
 }
